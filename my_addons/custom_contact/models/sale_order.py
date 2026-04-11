ac45bc94030264865335e7c9c5d986075d45cc96
trungnl <trungnl@dts.com.vn>
2026-04-11 16:41:04 +0700

    fix bug

---
+++ my_addons/custom_contact/models/sale_order.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_confirm(self):
+        res = super().action_confirm()
+
+        partners = self.mapped("partner_id").mapped("commercial_partner_id")
+        partners._update_customer_tier()
+
+        return res
